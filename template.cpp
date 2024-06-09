6e3908ae3b5e3fb084e8d92e6a3970f5ec417eb9
srvcsc <srvkrrr@gmail.com>
2024-06-09 09:10:36 +0530

    added template file

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+
+void solve();
+
+int32_t main() {
+    int tc = 1;
+    cin >> tc;
+    while (tc > 0) {
+        solve();
+        --tc;
+    }
+    return 0;
+}
+
+
+void solve() {
+    
+}
